dbc86f4628c4a305cba065f4de595ed0b1e9d892
harsh00410 <harsh4781.be23@chitkara.edu.in>
2024-09-15 14:27:29 +0530

    Add files via upload

---
+++ staffList.js
@@ -0,0 +1,33 @@
+import img1 from './1.jpeg';
+import img2 from './2.jpeg';
+import img3 from './6.jpeg';
+import { faker } from '@faker-js/faker'
+const staffList =[{
+    key:0,
+    avatar : img1,
+    name : 'Tiger',
+    description : 'It roars loud.',
+    star : '🌟5',
+    Author : 'Emily copper'
+},
+{
+    key:1,
+    avatar : img2,
+    name : 'Panda',
+    description : "Known for its sweetness",
+    star : '🌟5',
+    Author : 'halle copper'
+},
+{
+    key:2,
+    avatar : img3,
+    name : 'Giraffee',
+    description : "Nobody is on their level",
+    star : '🌟4',
+    Author : 'James Bond'
+},
+
+
+
+]
+export default staffList
---
+++ tutList.js
@@ -0,0 +1,31 @@
+import img4 from './3.jpeg'
+import img5 from './4.jpeg'
+import img6 from './5.jpeg'
+
+const tutList = [{
+    key:0,
+    avatar : img4,
+    name : 'Zebra',
+    description : 'Known for its black and white stripes.',
+    star : '🌟4',
+    Author : 'Prince William'
+},
+{
+    key:1,
+    avatar : img5,
+    name : 'Elephant',
+    description : "Greatest of all times",
+    star : '🌟5',
+    Author : 'Prince Andrei'
+},
+{
+    key:2,
+    avatar : img6,
+    name : 'HIPPOO',
+    description : "Wildest",
+    star : '🌟4',
+    Author : 'Vittorio Emanuele'
+},
+]
+
+export default tutList
